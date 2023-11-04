07fb4960284f8f1dcef1e41f8f10b00ad3f1812b
ymonbru <109665681+ymonbru@users.noreply.github.com>
2023-11-04 19:06:01 +0100

    Delete DC directory

--- DC/DC.lean
+++
@@ -1,7 +0,0 @@
--- This module serves as the root of the `DC` library.
--- Import modules here that should be built as part of the library.
-import «DC».Graph
---import «DC».GraphForSalamander
-import «DC».Grid
-import «DC».Print
-import «DC».Proof
